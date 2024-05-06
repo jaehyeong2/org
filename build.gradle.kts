8e4739289a4cff6f9923c9a0e9a5f90232852da1
jaehyeong2 <wogud1514@gmail.com>
2024-05-06 15:23:11 +0900

    chore: mockk gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.13.7")
 }
 
 tasks.withType<KotlinCompile> {
