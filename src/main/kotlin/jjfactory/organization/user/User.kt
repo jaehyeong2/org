53ad405aa9bed20b749237674f961eb42f46f7cb
jaehyeong2 <wogud1514@gmail.com>
2024-05-07 16:12:10 +0900

    chore: docker-compose / application.yml

--- src/main/kotlin/jjfactory/organization/user/User.kt
+++ src/main/kotlin/jjfactory/organization/user/User.kt
@@ -4,10 +4,12 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
 
+@Table(name = "users")
 @Entity
 class User(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
