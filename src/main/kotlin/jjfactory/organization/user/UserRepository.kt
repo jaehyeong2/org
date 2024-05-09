3389c55eb66ac4dd59730c81a724675a0fe5a772
jaehyeong2 <wogud1514@gmail.com>
2024-05-09 15:53:08 +0900

    fix: jpa repository query 메소드명 에러 수정

--- src/main/kotlin/jjfactory/organization/user/UserRepository.kt
+++ src/main/kotlin/jjfactory/organization/user/UserRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository: JpaRepository<User,Long> {
     fun existsByActivateToken(token: String): Boolean
-    fun findAllByOrganizationIdAndActivatedIsTrue(organizationId: Long): List<User>
+    fun findAllByOrganizationIdAndIsActivatedIsTrue(organizationId: Long): List<User>
 }
